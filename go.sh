298f5dd17a581cd3d930e9ddf146debf149ef8e1
Davide Guerri <davide.guerri@hp.com>
2014-10-11 16:49:10 +0100

    Workaround for Parallels parallel provisioning bug

--- go.sh
+++ go.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
-set -e 
+set -e
 
-vagrant up --no-provision
+# Parallel spawning of virtual machines won't work for Parallels
+# See https://github.com/Parallels/vagrant-parallels/issues/148
+vagrant up --no-provision --no-parallel
 vagrant provision controller
 

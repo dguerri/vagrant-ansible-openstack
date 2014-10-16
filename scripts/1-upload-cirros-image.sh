b85fc0951a435a2f639ed792f108dd1813f30a9d
John Dewey <john@dewey.ws>
2014-10-16 12:49:13 -0700

    Download image through a URL vs temporary download
    No need to temporarily download the image to discard it after import.

--- scripts/1-upload-cirros-image.sh
+++ scripts/1-upload-cirros-image.sh
@@ -1,22 +1,13 @@
 #!/bin/bash -e
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+IMAGE="http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img"
 
 . $DIR/openstack-admin-example.rc
 
-rm -rf $DIR/images
-mkdir $DIR/images
-cd $DIR/images
-
-#wget http://cdn.download.cirros-cloud.net/0.3.2/cirros-0.3.2-x86_64-disk.img
-wget http://cdn.download.cirros-cloud.net/0.3.3/cirros-0.3.3-x86_64-disk.img
-
 glance image-create --name "cirros-0.3.3-x86_64" \
-                    --file cirros-0.3.3-x86_64-disk.img \
+                    --copy-from $IMAGE \
                     --disk-format qcow2 \
                     --container-format bare \
                     --is-public True \
                     --progress
-
-cd -
-rm -rf $DIR/images
